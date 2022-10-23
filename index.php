f082b205985fd58eb0a77946bd01429321023122
Alexander <alexander_krupnin@mail.ru>
2022-10-23 20:28:53 +0300

    1

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <title>JS</title>
+      <link rel="manifest" href = "site.webmanifest">
+    <link rel="stylesheet" href = "index.css">
+    <link rel="stylesheet" href = "index.js">
+  </head>
+  <body>
+  <header>
+      
+     
+</form>
+    </main>
+    <footer>
+     
+    </footer>
+  </body>
+  </html>
