bee9139423e37fe48054ac55f7fc0a86d8c8d65c
Alexander <alexander_krupnin@mail.ru>
2022-10-23 21:04:40 +0300

    2

---
+++ index.js
@@ -0,0 +1,17 @@
+/*let number = +prompt('Введите число')
+let degree = +prompt('Введите степень')
+let answer = 1;
+for(let i = 0; i < degree; i++){
+    answer = answer * number;
+}
+alert(answer);*/
+    function pow() {
+        let number = +prompt('Введите число')
+    let degree = +prompt('Введите степень')
+    let answer = 1;
+    for(let i = 0; i < degree; i++){
+        answer = answer * number;
+    }
+    alert(answer);    location.reload() 
+    }pow() 
+    
