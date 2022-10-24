1f6b950ee9be3cf6b83f7c1916233aab2f25d402
Alexander <alexander_krupnin@mail.ru>
2022-10-25 00:06:38 +0300

    2

--- index.js
+++ index.js
@@ -1,17 +1,114 @@
-/*let number = +prompt('Введите число')
-let degree = +prompt('Введите степень')
-let answer = 1;
-for(let i = 0; i < degree; i++){
-    answer = answer * number;
-}
-alert(answer);*/
-    function pow() {
-        let number = +prompt('Введите число')
-    let degree = +prompt('Введите степень')
+function pow() {
+    let number = +prompt('Введите число');
+    let degree = +prompt('Введите степень');
     let answer = 1;
     for(let i = 0; i < degree; i++){
         answer = answer * number;
     }
-    alert(answer);    location.reload() 
-    }pow() 
-    
+    alert(answer);    
+    main();
+}
+//НОД
+function gcd() {
+    let number_1 = +prompt('Введите первое число');
+    let number_2 = +prompt('Введите второе число');
+    let answer = 1;
+    if(number_1 > 0 && number_2 > 0 ){
+    while (number_1 != number_2) {
+      if (number_1 > number_2) {
+        number_1 = number_1 - number_2;
+      }
+      else {
+        number_2 = number_2 - number_1;
+      }
+    }
+    answer = number_1;
+    alert(answer); 
+    }
+    else{
+        alert('Вы ввели отрицательное число или 0'); 
+    }      
+    main();
+}
+//наименьшая цифра
+function minDigit() {
+    let number = +prompt('Введите число');
+    let array = Array.from(number.toString(), Number)
+    console.log(array);
+    bubbleSortConcept(array);
+}
+
+function bubbleSortConcept(array) {
+    for (let j = array.length - 1; j > 0; j--) {
+      for (let i = 0; i < j; i++) {
+        if (array[i] > array[i + 1]) {
+          let temp = array[i];
+          array[i] = array[i + 1];
+          array[i + 1] = temp;
+        }
+      }
+    }
+    console.log(array);
+    alert(array[0]);
+    main();
+}
+//окончания
+function pluralizeRecords() {
+    let number = +prompt('Введите кол-во записей');
+    var string = number.toString();
+    var lastChar_two_symbols = string.slice(string.length - 2);
+    console.log(lastChar_two_symbols);
+
+    if(lastChar_two_symbols != 11 && lastChar_two_symbols != 12 && lastChar_two_symbols != 13 && lastChar_two_symbols != 14
+        && lastChar_two_symbols != 15 && lastChar_two_symbols != 16 
+        && lastChar_two_symbols != 17 && lastChar_two_symbols != 18
+        && lastChar_two_symbols != 19 && lastChar_two_symbols != 0){
+    var lastChar_one_symbols = string.slice(string.length - 1);
+
+    if(lastChar_one_symbols == 1){
+        alert('В результате выполнения запроса было найдено '+number+' запись');
+    }
+
+    if(lastChar_one_symbols == 2 || lastChar_one_symbols == 3 || lastChar_one_symbols == 4){
+        alert('В результате выполнения запроса было найдено '+number+' записи');
+    }
+
+    if(lastChar_one_symbols == 6 || lastChar_one_symbols == 7 || lastChar_one_symbols == 8 || lastChar_one_symbols == 9){
+        alert('В результате выполнения запроса было найдено '+number+' записей');
+    }
+    if(lastChar_one_symbols == 6 || lastChar_one_symbols == 7 || lastChar_one_symbols == 8 || lastChar_one_symbols == 9 || lastChar_one_symbols == 0){
+        alert('В результате выполнения запроса было найдено '+number+' записей');
+    }  
+    }else{
+        var lastChar_one_symbols = string.slice(string.length - 1);
+        if(lastChar_two_symbols => 11 && lastChar_two_symbols <= 19 && lastChar_two_symbols == 0){
+            alert('В результате выполнения запроса было найдено '+number+' записей');
+            }
+    }
+
+    main();
+}
+
+function main() {
+    let number_function = +prompt('Введите функцию: 1 - возведенеи в степень, 2 - НОД, 3 - наименьшая цифра, 4 - Pluralization, 5 - цисла Фибоначчи');
+    if(number_function == 1){
+        pow();
+    }
+    if(number_function == 2){
+        gcd();
+    }
+    if(number_function == 3){
+        minDigit();
+    }
+    if(number_function == 4){
+        pluralizeRecords();
+    }
+    if(number_function == 5){
+        fibb();
+    }
+    if(number_function > 5){
+        alert('Неправильный ввод');  
+        main();
+    }
+}
+main();
